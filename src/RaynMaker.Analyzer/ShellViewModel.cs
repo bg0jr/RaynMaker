fc225bc7e87f18fe8aa7d5a5c9e7f8e7e45f2bed
bg0jr <bg0jr@shared-files.de>
2015-05-29 14:45:00 +0200

    Analyzer: UI styling

--- src/RaynMaker.Analyzer/ShellViewModel.cs
+++ src/RaynMaker.Analyzer/ShellViewModel.cs
@@ -1,4 +1,8 @@
 ﻿using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Input;
+using Microsoft.Practices.Prism.Commands;
 using Microsoft.Practices.Prism.Mvvm;
 
 namespace RaynMaker.Analyzer
@@ -6,5 +10,25 @@ namespace RaynMaker.Analyzer
     [Export]
     class ShellViewModel : BindableBase
     {
+        public ShellViewModel()
+        {
+            NewCommand = new DelegateCommand( () => { } );
+            OpenCommand = new DelegateCommand( () => { } );
+            CloseCommand = new DelegateCommand( () => Application.Current.Shutdown() );
+            AboutCommand = new DelegateCommand( OnAbout );
+        }
+
+        public ICommand NewCommand { get; private set; }
+        
+        public ICommand OpenCommand { get; private set; }
+        
+        public ICommand CloseCommand { get; private set; }
+
+        public ICommand AboutCommand { get; private set; }
+    
+        private void OnAbout()
+        {
+            Process.Start( "https://github.com/bg0jr/RaynMaker" );
+        }
     }
 }
