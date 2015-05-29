0023c094e0f4697cbbe326f3a8161fdf6af3649d
bg0jr <bg0jr@shared-files.de>
2015-05-29 22:35:48 +0200

    DomainModel assemblies added

---
+++ src/RaynMaker.Entities.Persistancy/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RaynMaker.Entities.Persistancy")]
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a049b181-83f5-4afa-8260-1989adf2c1ed")]
---
+++ src/RaynMaker.Entities/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RaynMaker.Entities")]
+[assembly: ComVisible(false)]
+
+[assembly: Guid("304b6fd0-760f-4a56-b5c1-5f942960dbaa")]
