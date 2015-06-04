c84aa2ae64f5909359d3a8ddde89567188c8fdde
bg0jr <bg0jr@shared-files.de>
2015-06-04 14:35:15 +0200

    updated to Plainion.AppFw.Wpf 2.3 statusbar added

--- src/RaynMaker.Analyzer/Services/ProjectService.cs
+++ src/RaynMaker.Analyzer/Services/ProjectService.cs
@@ -1,18 +1,30 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Threading;
 using Plainion;
 using Plainion.AppFw.Wpf.Services;
+using Plainion.Progress;
 using RaynMaker.Infrastructure;
 
 namespace RaynMaker.Analyzer.Services
 {
     class ProjectService : ProjectService<Project>
     {
-        protected override Project Deserialize( string file )
+        protected override void InitializeProject( Project project, IProgress<IProgressInfo> progress, CancellationToken cancellationToken )
+        {
+            progress.Report( new UndefinedProgress( "Creating project at " + project.Location ));
+
+            Thread.Sleep( 30 * 1000 );
+
+            base.InitializeProject( project, progress, cancellationToken );
+        }
+
+        protected override Project Deserialize( string file, IProgress<IProgressInfo> progress, CancellationToken cancellationToken )
         {
             return new Project();
         }
 
-        protected override void Serialize( Project project )
+        protected override void Serialize( Project project, IProgress<IProgressInfo> progress, CancellationToken cancellationToken )
         {
             Contract.RequiresNotNullNotEmpty( project.Location, "project.Location" );
 
