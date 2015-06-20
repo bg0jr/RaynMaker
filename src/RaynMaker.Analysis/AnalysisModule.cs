e1f9c6228f8bd720b4a5be93af17e63a780c5ef4
bg0jr <bg0jr@shared-files.de>
2015-06-20 06:03:06 +0200

    Empty analysis module added

---
+++ src/RaynMaker.Analysis/AnalysisModule.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.Practices.Prism.MefExtensions.Modularity;
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+
+namespace RaynMaker.Analysis
+{
+    [ModuleExport( typeof( AnalysisModule ) )]
+    public class AnalysisModule : IModule
+    {
+        [Import]
+        public IRegionManager RegionManager { get; set; }
+
+        public void Initialize()
+        {
+        }
+    }
+}
