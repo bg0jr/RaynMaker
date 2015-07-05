b2825c9912ea500b6f336fca1ee011befd7f70d5
bg0jr <bg0jr@shared-files.de>
2015-07-05 15:00:17 +0200

    Blade: MarketCap added

---
+++ src/RaynMaker.Blade/DataSheetspec/InMioExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace RaynMaker.Blade.DataSheetSpec
+{
+    [MarkupExtensionReturnType( typeof( double ) )]
+    public class InMioExtension : MarkupExtension
+    {
+        public InMioExtension( double value )
+        {
+            Value = value;
+        }
+
+        [ConstructorArgument( "value" )]
+        public double Value { get; set; }
+
+        public override object ProvideValue( IServiceProvider serviceProvider )
+        {
+            return Value * 1000000;
+        }
+    }
+}
---
+++ src/RaynMaker.Blade/Engine/Providers/MarketCapProvider.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Plainion;
+using RaynMaker.Blade.DataSheetSpec;
+
+namespace RaynMaker.Blade.Engine
+{
+    public class MarketCapProvider : IFigureProvider
+    {
+        public string Name { get { return "MarketCap"; } }
+
+        public object ProvideValue( Asset asset )
+        {
+            var price = asset.Data.OfType<Price>().SingleOrDefault();
+            if( price == null )
+            {
+                return null;
+            }
+
+            var provider = new DatumProvider( asset );
+            var allShares = provider.GetSeries<SharesOutstanding>();
+
+            var shares = allShares.SingleOrDefault( d => d.Year == price.Date.Year );
+            if( shares == null )
+            {
+                shares = allShares.SingleOrDefault( d => d.Year == price.Date.Year - 1 );
+                if( shares == null )
+                {
+                    return null;
+                }
+            }
+
+            var result = new DerivedDatum
+            {
+                Date = price.Date,
+                Currency = price.Currency,
+                Value = shares.Value * price.Value
+            };
+            result.Inputs.Add( shares );
+            result.Inputs.Add( price );
+            return result;
+        }
+    }
+}
