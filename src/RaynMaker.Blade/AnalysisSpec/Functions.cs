89be9a1304f3a0c1ca8a90244876c87417012193
bg0jr <bg0jr@shared-files.de>
2015-08-10 17:28:57 +0200

    Growth function added

--- src/RaynMaker.Blade/AnalysisSpec/Functions.cs
+++ src/RaynMaker.Blade/AnalysisSpec/Functions.cs
@@ -14,14 +14,53 @@ namespace RaynMaker.Blade.AnalysisSpec
             };
 
             var currencyDatums = series.OfType<ICurrencyDatum>();
-            if ( currencyDatums.Any( ))
+            if( currencyDatums.Any() )
             {
                 result.Currency = currencyDatums.First().Currency;
             }
-            
+
             result.Inputs.AddRange( series );
-            
+
             return result;
+        }
+
+        /// <summary>
+        /// Returns growth rate in percentage.
+        /// </summary>
+        public static IDatum Growth( IEnumerable<IDatum> series )
+        {
+            IReadOnlyList<IDatum> sortedSeries = series
+                .OfType<IAnualDatum>()
+                .OrderBy( d => d.Year )
+                .ToList();
+
+            if( !sortedSeries.Any() )
+            {
+                sortedSeries = series
+                    .OfType<IDailyDatum>()
+                    .OrderBy( d => d.Date )
+                    .ToList();
+            }
+
+            var rates = new List<double>( sortedSeries.Count - 1 );
+            for( int i = 1; i < sortedSeries.Count; ++i )
+            {
+                rates.Add( Growth( sortedSeries[ i - 1 ].Value, sortedSeries[ i ].Value ) );
+            }
+
+            var result = new DerivedDatum()
+            {
+                Value = rates.Average()
+            };
+
+            result.Inputs.AddRange( series );
+
+            return result;
+        }
+
+        private static double Growth( double oldValue, double newValue )
+        {
+            return ( newValue - oldValue ) / oldValue * 100;
         }
 
         public static IEnumerable<IDatum> Last( IEnumerable<IDatum> series, int count )
