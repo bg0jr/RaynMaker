50ed7ec5cb6cda566d18e3edff291fd0b45df0b4
bg0jr <bg0jr@shared-files.de>
2015-08-13 15:18:07 +0200

    BarChart added (part 1)

---
+++ src/RaynMaker.Blade/AnalysisSpec/BarChart.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Windows.Controls.DataVisualization.Charting;
+using System.Windows.Documents;
+using RaynMaker.Blade.Engine;
+using RaynMaker.Blade.Entities;
+using DatumSeries = RaynMaker.Blade.DataSheetSpec.Series;
+
+namespace RaynMaker.Blade.AnalysisSpec
+{
+    public class BarChart : IReportElement
+    {
+        public string Caption { get; set; }
+
+        [Required]
+        public string Source { get; set; }
+
+        public void Report( ReportContext context )
+        {
+            var series = ( IDatumSeries )context.ProvideValue( Source ) ?? DatumSeries.Empty;
+            var caption = Caption ?? series.Name;
+
+            var chart = new Chart();
+            var barSeries = new BarSeries();
+            barSeries.Title = caption;
+            barSeries.ItemsSource = series;
+            barSeries.IndependentValuePath = "Period";
+            barSeries.DependentValuePath = "Value";
+
+            chart.Series.Add( barSeries );
+
+            context.Document.Blocks.Add( new BlockUIContainer( chart ) );
+        }
+    }
+}
