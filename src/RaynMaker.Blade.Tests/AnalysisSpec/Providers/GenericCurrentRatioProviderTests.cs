4da923269455b712e6a8dfdf15b33f0a5d282e72
bg0jr <bg0jr@shared-files.de>
2015-08-13 07:10:12 +0200

    more tests

--- src/RaynMaker.Blade.Tests/AnalysisSpec/Providers/GenericCurrentRatioProviderTests.cs
+++ src/RaynMaker.Blade.Tests/AnalysisSpec/Providers/GenericCurrentRatioProviderTests.cs
@@ -85,6 +85,38 @@ namespace RaynMaker.Blade.Tests.AnalysisSpec.Providers
             Assert.That( ex.Message, Is.StringContaining( "not frozen" ).And.StringContaining( RhsSeriesName ) );
         }
 
+        [Test]
+        public void ProvideValue_RhsHasNoDataForPeriod_MissingDataForPeriodReturned()
+        {
+            myLhsSeries = new Series( CreateDatum( 2015, 1 ) );
+            myLhsSeries.Freeze();
+            myRhsSeries = new Series( CreateDatum( 2014, 1 ) );
+            myRhsSeries.Freeze();
+
+            var result = myProvider.ProvideValue( myContext.Object );
+
+            Assert.That( result, Is.InstanceOf<MissingDataForPeriod>() );
+            Assert.That( ( ( MissingDataForPeriod )result ).Datum, Is.EqualTo( RhsSeriesName ) );
+        }
+
+        [Test]
+        public void ProvideValue_WithValidInputData_RatioReturned()
+        {
+            myLhsSeries = new Series( CreateDatum( 2015, 5 ), CreateDatum( 2014, 7 ) );
+            myLhsSeries.Freeze();
+            myRhsSeries = new Series( CreateDatum( 2015, 23 ), CreateDatum( 2014, 37 ) );
+            myRhsSeries.Freeze();
+
+            var result = ( IDatum )myProvider.ProvideValue( myContext.Object );
+
+            Assert.That( result.Period, Is.EqualTo( new YearPeriod( 2015 ) ) );
+            Assert.That( result.Value, Is.EqualTo( 28 ) );
+        }
+
+        // TODO: currency preserved
+        // TODO: inputs added
+        // TODO: inconsistent currencies
+
         private IDatum CreateDatum( int year, double value )
         {
             return new Datum
