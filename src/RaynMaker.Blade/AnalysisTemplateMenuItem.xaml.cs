8a34970f512a25e3335243065f82f392cfcf3810
bg0jr <bg0jr@shared-files.de>
2015-08-21 12:52:24 +0200

    AnalysisTemplateMenuItem.xaml added

---
+++ src/RaynMaker.Blade/AnalysisTemplateMenuItem.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.Composition;
+using System.Windows.Controls;
+
+namespace RaynMaker.Blade
+{
+    [Export]
+    public partial class AnalysisTemplateMenuItem : MenuItem
+    {
+        [ImportingConstructor]
+        public AnalysisTemplateMenuItem( AnalysisTemplateMenuItemModel model )
+        {
+            InitializeComponent();
+
+            DataContext = model;
+        }
+    }
+}
---
+++ src/RaynMaker.Blade/AnalysisTemplateMenuItemModel.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.Composition;
+using System.Windows.Input;
+using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.Interactivity.InteractionRequest;
+using Microsoft.Practices.Prism.Mvvm;
+using RaynMaker.Infrastructure;
+
+namespace RaynMaker.Blade
+{
+    [Export]
+    public class AnalysisTemplateMenuItemModel : BindableBase
+    {
+        private IProjectHost myProjectHost;
+
+        [ImportingConstructor]
+        public AnalysisTemplateMenuItemModel( IProjectHost projectHost )
+        {
+            myProjectHost = projectHost;
+            myProjectHost.Changed += myProjectHost_Changed;
+
+            InvokeCommand = new DelegateCommand( OnInvoke );
+            InvokeRequest = new InteractionRequest<INotification>();
+        }
+
+        public bool IsEnabled { get { return myProjectHost.Project != null; } }
+
+        void myProjectHost_Changed()
+        {
+            OnPropertyChanged( () => IsEnabled );
+        }
+
+        private void OnInvoke()
+        {
+            var notification = new Notification();
+            notification.Title = "Analysis template";
+
+            InvokeRequest.Raise( notification, c => { } );
+        }
+
+        public ICommand InvokeCommand { get; private set; }
+
+        public InteractionRequest<INotification> InvokeRequest { get; private set; }
+    }
+}
