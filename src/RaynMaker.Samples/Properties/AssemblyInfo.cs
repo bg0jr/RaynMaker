d9b8ce4d84572ac6656a48437b5f5d426dbfa27d
bg0jr <bg0jr@shared-files.de>
2015-08-26 13:31:45 +0200

    Separate project for samples added

---
+++ src/RaynMaker.Samples/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle( "RaynMaker.Samples" )]
+[assembly: ComVisible( false )]
+[assembly: Guid("8471447d-9d9e-4e5b-b37a-03d2cafd6b68")]
