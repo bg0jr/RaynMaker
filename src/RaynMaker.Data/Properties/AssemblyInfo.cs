2c00e2a3c20c3792abe96cb5e0db88e5991961bf
bg0jr <bg0jr@shared-files.de>
2015-08-27 06:48:46 +0200

    new assemblies created

---
+++ src/RaynMaker.Data/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Markup;
+
+[assembly: AssemblyTitle( "RaynMaker.Data" )]
+[assembly: ComVisible( false )]
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+    //(used if a resource is not found in the page, 
+    // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+    //(used if a resource is not found in the page, 
+    // app, or any theme specific resource dictionaries)
+)]
+
---
+++ src/RaynMaker.Entities.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle( "RaynMaker.Entities.Tests" )]
+[assembly: ComVisible( false )]
+[assembly: Guid("7a74bb31-a9a1-40a9-b6de-452aa3ed8b9a")]
