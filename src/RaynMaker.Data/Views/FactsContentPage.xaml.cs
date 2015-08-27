31f97490a5f79577d838ea16360ca9c3e6748f17
bg0jr <bg0jr@shared-files.de>
2015-08-27 07:40:46 +0200

    CP sorting fix

--- src/RaynMaker.Data/Views/FactsContentPage.xaml.cs
+++ src/RaynMaker.Data/Views/FactsContentPage.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.Composition;
 using System.Windows.Controls;
+using Microsoft.Practices.Prism.Regions;
 using RaynMaker.Blade.ViewModels;
 
 namespace RaynMaker.Blade.Views
 {
     [Export]
+    [ViewSortHint( "100" )]
     public partial class FactsContentPage : UserControl
     {
         [ImportingConstructor]
