073e60abb3be8396de7c05d8fe595573479f8c2b
bg0jr <bg0jr@shared-files.de>
2015-08-28 07:20:29 +0200

    Assemblies for "Import" module created

---
+++ src/RaynMaker.Import/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RaynMaker.Import")]
+[assembly: ComVisible(false)]
+[assembly: Guid("5ef8f3af-198f-42ca-b26f-a3916c625a01")]
