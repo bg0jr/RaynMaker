2f758df75907a639fb92979cb3197479ec86b303
bg0jr <bg0jr@shared-files.de>
2015-08-31 07:33:24 +0200

    minor cleanup

--- src/RaynMaker.Import/DatumProviderFactory.cs
+++ src/RaynMaker.Import/DatumProviderFactory.cs
@@ -1,52 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using RaynMaker.Import;
-using RaynMaker.Import.Providers;
+﻿using RaynMaker.Import.Providers;
 using RaynMaker.Import.Spec;
 
 namespace RaynMaker.Import
 {
     public class DatumProviderFactory : IDatumProviderFactory
     {
-        private IDocumentBrowser myWebScrapSC;
+        private IDocumentBrowser myBrowser;
 
-        public DatumProviderFactory( IDocumentBrowser webScrapSC, IFetchPolicy fetchPolicy )
-            : this( webScrapSC, fetchPolicy, null )
+        public DatumProviderFactory( IDocumentBrowser browser, IFetchPolicy fetchPolicy )
+            : this( browser, fetchPolicy, null )
         {
         }
 
-        public DatumProviderFactory( IDocumentBrowser webScrapSC, IFetchPolicy fetchPolicy, IResultPolicy resultPolicy )
+        public DatumProviderFactory( IDocumentBrowser browser, IFetchPolicy fetchPolicy, IResultPolicy resultPolicy )
         {
-            myWebScrapSC = webScrapSC;
+            myBrowser = browser;
             FetchPolicy = fetchPolicy;
             ResultPolicy = resultPolicy;
 
             LocatorRepository = new DatumLocatorRepository();
         }
 
-        public IFetchPolicy FetchPolicy
-        {
-            get;
-            private set;
-        }
+        public IFetchPolicy FetchPolicy { get; private set; }
 
-        public IResultPolicy ResultPolicy
-        {
-            get;
-            private set;
-        }
+        public IResultPolicy ResultPolicy { get; private set; }
 
         public IDatumProvider Create( DatumLocator datumLocator )
         {
-            return new GenericDatumProvider( myWebScrapSC, datumLocator, FetchPolicy, ResultPolicy );
+            return new GenericDatumProvider( myBrowser, datumLocator, FetchPolicy, ResultPolicy );
         }
 
         public IDatumProvider Create( string datum )
         {
             var locator = LocatorRepository.Get( datum );
-            if ( locator == null )
+            if( locator == null )
             {
                 return null;
             }
@@ -54,10 +41,6 @@ namespace RaynMaker.Import
             return Create( locator );
         }
 
-        public DatumLocatorRepository LocatorRepository
-        {
-            get;
-            private set;
-        }
+        public DatumLocatorRepository LocatorRepository { get; private set; }
     }
 }
