65bfd9c99f7b0a15ce3b91f8824e8b92d5ecf3ac
bg0jr <bg0jr@shared-files.de>
2015-09-06 14:34:54 +0200

    Edit of URLs simplified

--- src/RaynMaker.Import.Web/ViewModels/EditCaptureViewModel.cs
+++ src/RaynMaker.Import.Web/ViewModels/EditCaptureViewModel.cs
@@ -17,21 +17,10 @@ namespace RaynMaker.Import.Web.ViewModels
     {
         private IEnumerable<NavigatorUrl> myUrls;
         private NavigatorUrl mySelectedUrl;
-        private string mySelectedMacro;
-        private string myRegExp;
-        private string myRegExpTarget;
-        private string mySelectedText;
         private INotification myNotification;
 
         public EditCaptureViewModel()
         {
-            RegExpTarget = "{0}";
-            SelectedMacro = Macros.First();
-
-            InsertMacroCommand = new DelegateCommand( OnInsertMacro );
-            InsertRegExpCommand = new DelegateCommand( OnInsertRegExp );
-            InsertRegExpTargetCommand = new DelegateCommand( OnInsertRegExpTarget );
-
             OkCommand = new DelegateCommand( OnOk );
             CancelCommand = new DelegateCommand( OnCancel );
         }
@@ -46,53 +35,6 @@ namespace RaynMaker.Import.Web.ViewModels
         {
             get { return mySelectedUrl; }
             set { SetProperty( ref mySelectedUrl, value ); }
-        }
-
-        public string SelectedText
-        {
-            get { return mySelectedText; }
-            set { SetProperty( ref mySelectedText, value ); }
-        }
-
-        public IEnumerable<string> Macros { get { return new[] { "${stock.Isin}", "${stock.Wpkn}", "${stock.Symbol}" }; } }
-
-        public string SelectedMacro
-        {
-            get { return mySelectedMacro; }
-            set { SetProperty( ref mySelectedMacro, value ); }
-        }
-
-        public ICommand InsertMacroCommand { get; private set; }
-
-        private void OnInsertMacro()
-        {
-            SelectedText = SelectedMacro;
-        }
-
-        public string RegExp
-        {
-            get { return myRegExp; }
-            set { SetProperty( ref myRegExp, value ); }
-        }
-
-        public ICommand InsertRegExpCommand { get; private set; }
-
-        private void OnInsertRegExp()
-        {
-            SelectedText = "{" + RegExp + "}";
-        }
-
-        public string RegExpTarget
-        {
-            get { return myRegExpTarget; }
-            set { SetProperty( ref myRegExpTarget, value ); }
-        }
-
-        public ICommand InsertRegExpTargetCommand { get; private set; }
-
-        private void OnInsertRegExpTarget()
-        {
-            SelectedText = RegExpTarget;
         }
 
         public ICommand OkCommand { get; private set; }
