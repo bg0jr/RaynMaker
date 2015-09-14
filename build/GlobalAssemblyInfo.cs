4e2a4aae97ae6d5ba9e1f61b57a6e06a89bbb391
bg0jr <bg0jr@shared-files.de>
2015-09-14 20:36:18 +0200

    version increased

--- build/GlobalAssemblyInfo.cs
+++ build/GlobalAssemblyInfo.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 
 [assembly: AssemblyDescription( "RaynMaker" )]
 [assembly: AssemblyCopyright( "Copyright © 2015" )]
-[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyVersion( "1.1.0.0" )]
 [assembly: AssemblyProduct( "RaynMaker" )]
