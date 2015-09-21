373a62d8c267fea5e6e3007e4d31f6bb8d13bfc4
bg0jr <bg0jr@shared-files.de>
2015-09-21 22:41:59 +0200

    AutoCompleteRichTextBox improved

--- src/RaynMaker.Infrastructure/Controls/DocumentUtils.cs
+++ src/RaynMaker.Infrastructure/Controls/DocumentUtils.cs
@@ -1,9 +1,49 @@
-﻿using System.Windows.Documents;
+﻿using System.Windows;
+using System.Windows.Documents;
 
 namespace RaynMaker.Infrastructure.Controls
 {
-    public static class WordBreaker
+    public static class DocumentUtils
     {
+        // Helper that returns true when passed property applies to Hyperlink only.
+        public static bool IsHyperlinkProperty( DependencyProperty dp )
+        {
+            return dp == Hyperlink.CommandProperty ||
+                dp == Hyperlink.CommandParameterProperty ||
+                dp == Hyperlink.CommandTargetProperty ||
+                dp == Hyperlink.NavigateUriProperty ||
+                dp == Hyperlink.TargetNameProperty;
+        }
+
+        // Helper that returns true if passed caretPosition and backspacePosition cross a hyperlink end boundary
+        // (under the assumption that caretPosition and backSpacePosition are adjacent insertion positions).
+        public static bool IsHyperlinkBoundaryCrossed( TextPointer caretPosition, TextPointer backspacePosition, out Hyperlink backspacePositionHyperlink )
+        {
+            var caretPositionHyperlink = GetHyperlinkAncestor( caretPosition );
+            backspacePositionHyperlink = GetHyperlinkAncestor( backspacePosition );
+
+            return ( caretPositionHyperlink == null && backspacePositionHyperlink != null ) ||
+                ( caretPositionHyperlink != null && backspacePositionHyperlink != null && caretPositionHyperlink != backspacePositionHyperlink );
+        }
+
+        // Helper that returns a Hyperlink ancestor of passed TextPointer.
+        private static Hyperlink GetHyperlinkAncestor( TextPointer position )
+        {
+            var parent = position.Parent as Inline;
+            while( parent != null && !( parent is Hyperlink ) )
+            {
+                parent = parent.Parent as Inline;
+            }
+
+            return parent as Hyperlink;
+        }
+
+        // Helper that returns true when passed TextPointer is within the scope of a Hyperlink element.
+        public static bool IsInHyperlinkScope( TextPointer position )
+        {
+            return GetHyperlinkAncestor( position ) != null;
+        }
+
         /// <summary>
         /// Returns a TextRange covering a word containing or following this TextPointer.
         /// </summary>
