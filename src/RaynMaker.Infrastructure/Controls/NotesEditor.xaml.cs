43709c3d79c1abb10c4220c1941d610f315f6dd9
bg0jr <bg0jr@shared-files.de>
2015-09-22 20:44:54 +0200

    NotesEditor: Headline added

--- src/RaynMaker.Infrastructure/Controls/NotesEditor.xaml.cs
+++ src/RaynMaker.Infrastructure/Controls/NotesEditor.xaml.cs
@@ -42,6 +42,23 @@ namespace RaynMaker.Infrastructure.Controls
 
             temp = myEditor.Selection.GetPropertyValue( Inline.TextDecorationsProperty );
             myUnderline.IsChecked = ( temp != DependencyProperty.UnsetValue ) && ( temp.Equals( TextDecorations.Underline ) );
+        
+            temp = myEditor.Selection.GetPropertyValue( Inline.FontSizeProperty );
+            myHeadline.IsChecked = ( temp != DependencyProperty.UnsetValue ) && ( temp.Equals( 20d ) );
+        }
+
+        private void OnHeadlineClick( object sender, RoutedEventArgs e )
+        {
+            if( myHeadline.IsChecked == true )
+            {
+                myEditor.Selection.ApplyPropertyValue( Inline.FontSizeProperty, 20d );
+                myEditor.Selection.ApplyPropertyValue( Inline.FontWeightProperty, FontWeights.Bold );
+            }
+            else
+            {
+                myEditor.Selection.ApplyPropertyValue( Inline.FontSizeProperty, 13d );
+                myEditor.Selection.ApplyPropertyValue( Inline.FontWeightProperty, FontWeights.Normal );
+            }
         }
     }
 }
