86f621c71942e6bd3386d5fd6975e9cf0340a884
bg0jr <bg0jr@shared-files.de>
2015-10-25 13:35:48 +0100

    Ticker continued

---
+++ src/RaynMaker.Data/ViewModels/TickerEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RaynMaker.Entities;
+
+namespace RaynMaker.Data.ViewModels
+{
+    class TickerEntry
+    {
+        private Stock myStock;
+
+        public TickerEntry( Stock stock )
+        {
+            myStock = stock;
+        }
+
+        public string Company { get { return myStock.Company.Name; } }
+
+        public string Isin { get { return myStock.Isin; } }
+    }
+}
--- src/RaynMaker.Data/ViewModels/TickerViewModel.cs
+++ src/RaynMaker.Data/ViewModels/TickerViewModel.cs
@@ -1,10 +1,121 @@
-﻿using System.ComponentModel.Composition;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.Interactivity.InteractionRequest;
 using Microsoft.Practices.Prism.Mvvm;
+using RaynMaker.Data.Services;
+using RaynMaker.Entities;
+using RaynMaker.Entities.Datums;
+using RaynMaker.Infrastructure;
+using RaynMaker.Infrastructure.Services;
 
 namespace RaynMaker.Data.ViewModels
 {
     [Export]
-    class TickerViewModel : BindableBase
+    class TickerViewModel : BindableBase, IInteractionRequestAware
     {
+        private IProjectHost myProjectHost;
+
+        [ImportingConstructor]
+        public TickerViewModel( IProjectHost projectHost )
+        {
+            myProjectHost = projectHost;
+
+            UpdateAllCommand = new DelegateCommand( OnUpdateAll, CanUpdateAll );
+            OkCommand = new DelegateCommand( OnOk );
+            CancelCommand = new DelegateCommand( OnCancel );
+
+            TickerEntries = new ObservableCollection<TickerEntry>();
+            
+            myProjectHost.Changed += OnProjectChanged;
+            OnProjectChanged();
+        }
+
+        private void OnProjectChanged()
+        {
+            if( myProjectHost.Project == null )
+            {
+                return;
+            }
+
+            TickerEntries.Clear();
+
+            var ctx = myProjectHost.Project.GetAssetsContext();
+            foreach( var stock in ctx.Stocks )
+            {
+                TickerEntries.Add( new TickerEntry( stock ) );
+            }
+        }
+
+        [Import( AllowDefault = true )]
+        public IDataProvider DataProvider { get; set; }
+        
+        public ObservableCollection<TickerEntry> TickerEntries { get; private set; }
+
+        public Action FinishInteraction { get; set; }
+
+        public INotification Notification { get; set; }
+
+        public ICommand OkCommand { get; private set; }
+
+        private void OnOk()
+        {
+            var ctx = myProjectHost.Project.GetAssetsContext();
+            ctx.SaveChanges();
+
+            FinishInteraction();
+        }
+
+        public ICommand CancelCommand { get; private set; }
+
+        private void OnCancel()
+        {
+            FinishInteraction();
+        }
+
+        public DelegateCommand UpdateAllCommand { get; private set; }
+
+        private bool CanUpdateAll()
+        {
+            return DataProvider != null;// && DataProvider.CanFetch( typeof( Price ) );
+        }
+
+        private void OnUpdateAll()
+        {
+        //    var today = DateTime.Today;
+
+        //    var series = new ObservableCollection<IDatum>();
+        //    WeakEventManager<ObservableCollection<IDatum>, NotifyCollectionChangedEventArgs>.AddHandler( series, "CollectionChanged", OnSeriesChanged );
+
+        //    // fetch some more data because of weekends and public holidays
+        //    // we will then take last one
+
+        //    DataProvider.Fetch( Stock,
+        //        typeof( Price ),
+        //        series,
+        //        new DayPeriod( today.Subtract( TimeSpan.FromDays( 7 ) ) ),
+        //        new DayPeriod( today.AddDays( 1 ) ) );
+        //}
+
+        //private void OnSeriesChanged( object sender, NotifyCollectionChangedEventArgs e )
+        //{
+        //    // there might be no privider - check for null
+        //    var price = ( Price )( ( IEnumerable<IDatum> )sender )
+        //        .OrderByDescending( d => d.Period )
+        //        .FirstOrDefault();
+
+        //    if( price != null )
+        //    {
+        //        Price.Value = price.Value;
+        //        Price.Currency = price.Currency;
+        //        Price.Period = price.Period;
+        //        Price.Source = price.Source;
+        //    }
+        }
     }
 }
