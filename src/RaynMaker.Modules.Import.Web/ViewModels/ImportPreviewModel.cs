9d0431d6911dce4d0840d256104bc385ac393baa
bg0jr <bg0jr@shared-files.de>
2016-01-06 21:36:40 +0100

    backlog updated

--- src/RaynMaker.Modules.Import.Web/ViewModels/ImportPreviewModel.cs
+++ src/RaynMaker.Modules.Import.Web/ViewModels/ImportPreviewModel.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Data;
 using System.Linq;
 using System.Windows.Input;
 using Microsoft.Practices.Prism.Commands;
